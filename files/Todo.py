5e2d7df793e653c9aa3d667372093b3ca418808c
zuhaibnoor <zuhaibnoor5@gmail.com>
2024-03-31 02:11:27 +0500

    added functionality to display the entered tasks/notes on the scrollframe

--- files/Todo.py
+++ files/Todo.py
@@ -1,11 +1,15 @@
 from customtkinter import *
 from CTkMessagebox import CTkMessagebox
 
+_row = 0
 def add_task(scroll_frame, entry):
-    task = entry.get()
+    task = entry.get().strip()
     if  task != '':
-        entry.delete(0,END)
         print(task)
-        task_label = CTkLabel(scroll_frame, text= task)
-        
-        
+        task_label = CTkLabel(scroll_frame, text = task)
+        # _column = 0
+        global _row
+        task_label.grid(row = _row, column= 0)
+        _row += 1
+    entry.delete(0,END)
+
